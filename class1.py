b67233ff76d67afdc48ed10f235b44015889bb5e
Adelsgit <adelkk.154@gmail.com>
2024-02-23 20:25:01 +0600

    Chechout

---
+++ class1.py
@@ -0,0 +1,5 @@
+class Hello:
+    def __init__(self, str):
+        self.str = str
+
+class Checkout(Hello):...
--- task.py
+++ task.py
@@ -1,3 +1,10 @@
 from art import tprint
+
 tprint("PYTHON")
 
+from class1 import Checkout
+
+q = Checkout('rty')
+
+
+tprint(q.str)
